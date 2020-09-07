322b8eef9ecea3d4c6bb29fbb9df9f0065960d87
yoalero <55332765+yoaroib@users.noreply.github.com>
2020-09-06 22:54:01 -0500

    Final Stage

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+
+// require and use "multer"...
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' });
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+app.post('/api/fileanalyse', upload.single('upfile') ,(req, res) => {
+  let up = req.file
+  res.json({ name: up.originalname, type: up.mimetype , size: up.size })
+})
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
